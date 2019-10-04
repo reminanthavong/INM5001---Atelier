f7fdde9c08f18260fb7fb41822e618e824cd8c5a
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-04 15:05:06 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,6 @@ const path = require('path')
 
 const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.listen(process.env.PORT || 5000, () => {
     log.info("app running");
