c302b452c88bd8f1d2b5f40e606b6df9dad492fe
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-14 19:03:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
 const { Pool } = require('pg');
 const pool = new Pool({
-  //connectionString: 'postgres://moura:27051989.mL@localhost:53081/',
-   connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
-
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -18,7 +15,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM persons');
+      const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
@@ -27,34 +24,4 @@ express()
       res.send("Error " + err);
     }
   })
-
-  //Page Mourad//
-  .get('/AffichageHoraire', async (req, res) => {
-      try {
-        const client = await pool.connect()
-        const results = await affichagehoraire(client);
-        const choix = await choixsemaine(client);
-        
-      } catch (err) {
-        console.error(err);
-        res.send("Error1" + err);
-      }
-    })
-
-//Fonctions Mourad//
-async function affichagehoraire (client) {
-        const result = await client.query(`SELECT *
-                                           FROM
-                                           (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
-                                                                                      	FROM TableHoraire TH
-                                                                                      	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
-                                                                                      	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
-                                                                                      	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                      	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
-                                                                                      ) AS SourceTable
-                                           `);
-        const results = { 'results': (result) ? result.rows : null};
-return results;
-}
-
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
