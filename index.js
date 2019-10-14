34ecf53602f7805f7cdbf80a8f59d52873f88f9b
Mourad Latoui <kh591283@ens.uqam.ca>
2019-10-14 16:05:15 -0400

    ajout mourad test

--- index.js
+++ index.js
@@ -25,4 +25,40 @@ express()
       res.send("Error " + err);
     }
   })
+
+  //Page Mourad//
+  .get('/AffichageHoraire', async (req, res) => {
+      try {
+        const client = await pool.connect()
+        const results = await affichagehoraire(client);
+        res.render('pages/AffichageHoraire', results );
+        client.release();
+
+
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//Fonctions Mourad//
+async function affichagehoraire (client) {
+        const result = await client.query(`SELECT * FROM (
+                                           	SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
+                                           	FROM TableHoraire TH
+                                           	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
+                                           	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
+                                           	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
+                                           	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
+                                           )
+                                           PIVOT(
+                                           	MAX(TypeQuart)
+                                           	FOR JourSemaine IN(
+                                           		LUNDI,MARDI,MERCREDI,JEUDI,VENDREDI
+                                           	)
+                                           )`);
+        const results = { 'results': (result) ? result.rows : null};
+return results;
+}
+//***************//
