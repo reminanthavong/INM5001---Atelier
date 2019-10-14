bdfea710b888979244a1b812959ef8871c6ef7be
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-14 18:56:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const pool = new Pool({
    connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+//Fonctions Mourad//
+async function affichagehoraire (client) {
+        const result = await client.query(`SELECT *
+                                           FROM
+                                           (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
+                                                                                      	FROM TableHoraire TH
+                                                                                      	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
+                                                                                      	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
+                                                                                      	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
+                                                                                      	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
+                                                                                      ) AS SourceTable
+                                           `);
+        const results = { 'results': (result) ? result.rows : null};
+return results;
+}
+async function choixsemaine (client) {
+        const result = await client.query(`SELECT DISTINCT IDTableHoraire FROM TableHoraire;`);
+        const results = { 'results': (result) ? result.rows : null};
+return results;
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
