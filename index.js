55a56db4fff0f7088aa20e886f45ec8a6d81d441
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-14 18:54:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,41 +28,5 @@ express()
   })
 
   //Page Mourad//
-  .get('/AffichageHoraire', async (req, res) => {
-      try {
-        const client = await pool.connect()
-        const results = await affichagehoraire(client);
-        const choix = await choixsemaine(client);
-        const choixsemaine = { 'choixsemaine': (choix) ? result.rows : null};
-        res.render('pages/AffichageHoraire', results, choix );
-        client.release();
-
-
-      } catch (err) {
-        console.error(err);
-        res.send("Error1" + err);
-      }
-    })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-//Fonctions Mourad//
-async function affichagehoraire (client) {
-        const result = await client.query(`SELECT *
-                                           FROM
-                                           (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
-                                                                                      	FROM TableHoraire TH
-                                                                                      	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
-                                                                                      	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
-                                                                                      	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                      	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
-                                                                                      ) AS SourceTable
-                                           `);
-        const results = { 'results': (result) ? result.rows : null};
-return results;
-}
-async function choixsemaine (client) {
-        const result = await client.query(`SELECT DISTINCT IDTableHoraire FROM TableHoraire;`);
-        const results = { 'results': (result) ? result.rows : null};
-return results;
-}
-//***************//
+  
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
