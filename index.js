d3bc2536d35928ff2ee9317937fbeb31adebbfd6
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-14 19:31:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,18 @@ express()
   .get('/AffichageHoraire', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await affichagehoraire(client);
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/AffichageHoraire', results );
+      const getHoraire = await client.query(`SELECT *
+                                           FROM
+                                           (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
+                                                                                      	FROM TableHoraire TH
+                                                                                      	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
+                                                                                      	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
+                                                                                      	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
+                                                                                      	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
+                                                                                      ) AS SourceTable
+                                           `);
+      const getHoraires = { 'getHoraires': (getHoraire) ? getHoraire.rows : null};
+      res.render('pages/AffichageHoraire', getHoraires );
       client.release();
     } catch (err) {
       console.error(err);
@@ -44,19 +53,3 @@ express()
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-async function affichagehoraire (client) {
-        const result = await client.query(`SELECT *
-                                           FROM
-                                           (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,BE.PrenomEmploye) AS NomEmploye
-                                                                                      	FROM TableHoraire TH
-                                                                                      	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
-                                                                                      	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
-                                                                                      	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                      	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
-                                                                                      ) AS SourceTable
-                                           `);
-        const results = { 'results': (result) ? result.rows : null};
-return results;
-}
