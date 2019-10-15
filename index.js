92cd21e495b1de4463b308353248041f39834306
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-14 20:03:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ express()
       const choixSemaine = await client.query(`SELECT DISTINCT IDTableHoraire FROM TableHoraire;`);
       const choixSemaines = { 'choixSemaines': (choixSemaine) ? choixSemaine.rows : null};
       
-      res.render('pages/AffichageHoraire',JSON.stringify(choixSemaines, getHoraires) );
+      res.render('pages/AffichageHoraire', getHoraires );
       client.release();
     } catch (err) {
       console.error(err);
