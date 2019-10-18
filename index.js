069cc2ff07bfeca6322accb176f164721f51c2b8
Mourad Latoui <kh591283@ens.uqam.ca>
2019-10-17 22:45:52 -0400

    affichage selon choix de semaine

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ express()
           }
    })
     .post('/api/v1/horaires', async (req, res) => {
-        const {choixsemaine} = req.body;
+        const {choixsemaine} = req.body; //{$choixsemaine}
         const employeur = 'Gestion3525'
          try {
               const client = await pool.connect()
@@ -50,7 +50,7 @@ express()
                                                                                                     	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
                                                                                                     	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
                                                                                                     	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                                    	WHERE TH.IDTableHoraire='{$choixsemaine}' AND TH.IDEmployeur='{$employeur}'
+                                                                                                    	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='{$employeur}'
                                                                                                     ) AS SourceTable
                                                          `);
                res.json( horaires );
