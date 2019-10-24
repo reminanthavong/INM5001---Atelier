79459dd81fd083396841a4020f756b329406b01d
Mourad Latoui <kh591283@ens.uqam.ca>
2019-10-24 19:56:17 -0400

    ajout reponse

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -32,7 +32,7 @@ const fonctions2  = async (req, res) => {
                                                                                                     	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
                                                                                                     	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
                                                                                                     	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                                    	WHERE TH.IDTableHoraire='${resp.choixsemaine}' AND TH.IDEmployeur='${employeur}'
+                                                                                                    	WHERE TH.IDTableHoraire='001' AND TH.IDEmployeur='Gestion3525'
                                                                                                     ) AS SourceTable;
                                                          `);
                const horairesRecu = { 'horaires': (horaires) ? horaires.rows : null};
