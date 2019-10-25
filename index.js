35a23e7f2ba35edb22a788c22dc6756a2f879f40
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-25 15:10:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ const { Pool } = require('pg');
 //});
 
 const pool = new Pool({
-    user: "nvgnyxzoglgozi",
-    password: "e669132b12a9be74fc1c2d60d357928740f17fc9e2b84c1d4b30199b3bb1bd14",
-    host: "ec2-54-243-208-234.compute-1.amazonaws.com",
-    port: 5432,
-    database: "d181pdml81daoa"
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 // Linker queries.js
