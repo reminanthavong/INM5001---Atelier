b59653c7434492fa2024ddb4f2b8382992602f34
Argeeno <cfnta.lauzon@gmail.com>
2019-10-26 21:11:38 -0400

    correction fonction ajoutHoraire

--- gestionhoraire.js
+++ gestionhoraire.js
@@ -38,15 +38,18 @@ const enleverHoraire   = async (req, res) => {
 async function ajoutHoraire(employejour, employesoir, employenuit, datehoraire) {
   var jourSemaine = 1;
   var nombreEmploye = 1;
+
+  //Va chercher l'Idtablehoraire la plus élevé de la table idHoraire et ajoute 1 (logique possiblement erronné)
   var idHoraire;
   try{
     const client1 = await pool.connect();
-    idHoraire = await client.query('SELECT MAX(Idtablehoraire) FROM TableHoraire');
+    idHoraire = await client.query('SELECT MAX(Idtablehoraire) FROM TableHoraire') + 1;
     client1.release();
   } catch(e){
     return false
   }
 
+  //2 boucle pour l'insertion des employés pour l'horaire de jour, 1 boucle pour le nombre d'employer demander, 2ième boucle pour les jours de la semaine de 1 à 7
   try {
     while(nombreEmploye <= employejour){
       while(jourSemaine <= 7) {	
@@ -58,10 +61,38 @@ async function ajoutHoraire(employejour, employesoir, employenuit, datehoraire) 
     }
     nombreEmploye = nombreEmploye + 1;
   }
-  return true;
-  }  catch(e){
-  return false;
+
+  //2 boucle pour l'insertion des employés pour l'horaire de soir, 1 boucle pour le nombre d'employer demander, 2ième boucle pour les jours de la semaine de 1 à 7
+  jourSemaine = 1; nombreEmploye = 1;
+    while(nombreEmploye <= employejour){
+      while(jourSemaine <= 7) {	
+        const client = await pool.connect();
+        await client.query('INSERT INTO TableHoraire(Idtablehoraire, Dateparam, Idemployeur, Joursemaine, Typequart) values ($1, $2, $3, $4)', [idHoraire, datehoraire, 'Gestion8768', jourSemaine, 'S']);
+        await client.query('INSERT INTO TableHoraire(Idemploye) SELECT * FROM BaseQuartsEmploye LIMIT 1 WHERE BaseQuartsEmployes.Disponibilite="1" AND Typequart="S"'); 
+        client.release(); 
+        jourSemaine = jourSemaine + 1;  
+    }
+      nombreEmploye = nombreEmploye + 1;
   }
+    //2 boucle pour l'insertion des employés pour l'horaire de nuit, 1 boucle pour le nombre d'employer demander, 2ième boucle pour les jours de la semaine de 1 à 7
+    jourSemaine = 1; nombreEmploye = 1;
+    while(nombreEmploye <= employejour){
+      while(jourSemaine <= 7) {	
+        const client = await pool.connect();
+        await client.query('INSERT INTO TableHoraire(Idtablehoraire, Dateparam, Idemployeur, Joursemaine, Typequart) values ($1, $2, $3, $4)', [idHoraire, datehoraire, 'Gestion8768', jourSemaine, 'N']);
+        await client.query('INSERT INTO TableHoraire(Idemploye) SELECT * FROM BaseQuartsEmploye LIMIT 1 WHERE BaseQuartsEmployes.Disponibilite="1" AND Typequart="N"'); 
+        client.release(); 
+        jourSemaine = jourSemaine + 1;  
+    }
+    nombreEmploye = nombreEmploye + 1;
+  }
+  return true;
+  
+}  catch(e){
+  return false;
+}
+  
+  
 }
 
 module.exports = {
