a596571929e1c06eea26f60806c39dce7f65048b
Argeeno <cfnta.lauzon@gmail.com>
2019-10-26 20:40:46 -0400

    ajout fonction de génération d'horaire

--- gestionhoraire.js
+++ gestionhoraire.js
@@ -18,15 +18,56 @@ res.end();
 }
    
 const ajouterHoraire   = async (req, res) => {
-  res.end();
+	  try{	
+		  await ajoutHoraire(reqJson.employejour, reqJson.employesoir, reqJson.employenuit, reqJson.datehoraire);
+		  const reqJson = req.body;
+		  result.success = true;
+	  } catch (e) {
+		  result.success = false;
+	  } finally {
+		  res.setHeader("content-type", "application/json")
+		  res.send(JSON.stringify(result))
+	  }
 }
 
 const enleverHoraire   = async (req, res) => {
  res.end();
 }
+
+
+async function ajoutHoraire(employejour, employesoir, employenuit, datehoraire) {
+  var jourSemaine = 1;
+  var nombreEmploye = 1;
+  var idHoraire;
+  try{
+    const client1 = await pool.connect();
+    idHoraire = await client.query('SELECT MAX(Idtablehoraire) FROM TableHoraire');
+    client1.release();
+  } catch(e){
+    return false
+  }
+
+  try {
+    while(nombreEmploye <= employejour){
+      while(jourSemaine <= 7) {	
+        const client = await pool.connect();
+        await client.query('INSERT INTO TableHoraire(Idtablehoraire, Dateparam, Idemployeur, Joursemaine, Typequart) values ($1, $2, $3, $4)', [idHoraire, datehoraire, 'Gestion8768', jourSemaine, 'J']);
+        await client.query('INSERT INTO TableHoraire(Idemploye) SELECT * FROM BaseQuartsEmploye LIMIT 1 WHERE BaseQuartsEmployes.Disponibilite="1" AND Typequart="J"'); 
+        client.release(); 
+        jourSemaine = jourSemaine + 1;  
+    }
+    nombreEmploye = nombreEmploye + 1;
+  }
+  return true;
+  }  catch(e){
+  return false;
+  }
+}
+
 module.exports = {
   pageWeb,
   afficherHoraire,
   ajouterHoraire ,
   enleverHoraire 
 }
+
