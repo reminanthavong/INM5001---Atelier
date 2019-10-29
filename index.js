9d1e719d9722143742df90e4fd218dd315c25922
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-28 21:46:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express')
-<<<<<<< HEAD
 const session = require('express-session');
 const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const request = require('request');
-=======
-const path = require('path')
-const PORT = process.env.PORT || 5000
->>>>>>> master
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
-<<<<<<< HEAD
 // Linker queries.js
 const afficherhoraire = require('./afficherhoraire')
 const login = require('./login')
@@ -101,23 +95,3 @@ if (req.session.typeutilisateur == 1){
 
 
  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-=======
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
->>>>>>> master
