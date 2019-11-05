13f9e32c6fbfacd7b0189fc50f0f05d741856f41
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-04 19:10:58 -0500

    Update afficherhoraire.js

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -24,7 +24,7 @@ const fonctions2  = async (req, res) => {
         const choix = resp['choixsemaine']
         console.log(req)
              console.log(choix)
-        const employeur = req.session.username//'Gestion3525' //
+        const employeur = 'JNASH' //req.session.username//'Gestion3525' //
          try {
             const client = await pool.connect()
             const horaires = await client.query(`SELECT *
