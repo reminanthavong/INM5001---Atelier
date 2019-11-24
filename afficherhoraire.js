2bd3aefbb6aa01a67e5b807780ab629d93a85e99
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-24 18:01:16 -0500

    Update afficherhoraire.js

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -58,9 +58,36 @@ const fonctions3  = async (req, res) => {
 const fonctions4  = async (req, res) => {
   res.sendFile(path.join(__dirname+'/views/pages/AffichageHoraire.html' /*, getHoraires */));
 }
+
+const DonneesEmployes = await client.query(`SELECT IDEmploye, JourSemaine, TypeQuart
+FROM (
+SELECT *,ROW_NUMBER()OVER(PARTITION BY A.IDEmploye ORDER BY A.JourSemaine ASC) AS MaxSem	
+FROM(
+	SELECT BQE.IDEmploye, BQE.JourSemaine, BQE.TypeQuart, nbrQuartsmax ,ROW_NUMBER()OVER(PARTITION BY BQE.IDEmploye, BQE.JourSemaine ORDER BY BQE.TypeQuart ASC) AS MaxJOur
+	FROM basequartsemploye BQE
+	INNER JOIN baseemployes BE ON BE.IDEmploye=BQE.IDEmploye 
+		AND BE.IDEmployeur='Gestion8768'
+  		AND Disponibilite='1'
+  		AND ParamType='1'
+	LEFT JOIN Tableconges TC ON TC.IDEmploye=BQE.IDEmploye 
+		AND TC.JourSemaine=BQE.JourSemaine 
+		AND TC.TypeQuart=BQE.TypeQuart
+		AND TC.DateConges='2019-10-7'
+	WHERE TC.IDEmploye IS NULL 
+	ORDER BY DateEmbauche DESC, JourSemaine ASC, TypeQuart ASC
+	
+)A	
+WHERE MaxJour=1
+)B
+WHERE MaxSem<=nbrQuartsmax`);
+
+console.log(DonneesEmployes)
+
+
 module.exports = {
   fonctions1,
   fonctions2,
   fonctions3,
-  fonctions4
+  fonctions4,
+  DonneesEmployes
 }
