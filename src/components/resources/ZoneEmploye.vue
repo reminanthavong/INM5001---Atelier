635636259303aa8302892f14ead8e5bf4a0e0f7f
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-11-27 14:18:36 -0500

    Update ZoneEmploye.vue

--- src/components/resources/ZoneEmploye.vue
+++ src/components/resources/ZoneEmploye.vue
@@ -112,7 +112,7 @@
                         }).catch(error => {
                             console.log(error);
                         });
-                        this.$router.push("/")
+                        this.$router.push("/zoneEmploye")
 
                 },
                 demanderConge() {
@@ -139,7 +139,7 @@
                         }).catch(error => {
                             console.log(error);
                         });
-                        this.$router.push("/")
+                        this.$router.push("/zoneEmploye")
                 }
 
         }
