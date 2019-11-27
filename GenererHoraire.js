bb5af0e3b7107ad8bf37fabff06e98d08ad96127
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-27 13:27:58 -0500

    Update GenererHoraire.js

--- GenererHoraire.js
+++ GenererHoraire.js
@@ -4,18 +4,17 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
-
-
 //---------------------------------------------------------------------------------------------------------------------------------//
 
 const GenererHoraire = async (req, res) => {
 
 const employeur = req.session.username
 const choixsemaine = resp['choixsemaine'] || '000';
+const choixdate = resp['choixdate'] || '01/01/1899';
 
 try {
 	const client = await pool.connect()
-	const horaire = await client.query(`SELECT DISTINCT '999' AS IDTableHoraire, '10/7/2019' AS DateParam ,C.IDEmployeur,C.IDEmploye, C.JourSemaine, C.TypeQuart,c.Selection--,NBREmployes
+	const horaire = await client.query(`SELECT DISTINCT '${choixsemaine}' AS IDTableHoraire, '${choixdate}' AS DateParam ,C.IDEmployeur,C.IDEmploye, C.JourSemaine, C.TypeQuart,c.Selection--,NBREmployes
 	FROM(
 	SELECT IDEmploye,IDEmployeur,JourSemaine, TypeQuart,Selection,DateEmbauche
 	FROM (
@@ -26,13 +25,13 @@ try {
 			SELECT BQE.*, nbrQuartsmax,DateEmbauche ,ROW_NUMBER()OVER(PARTITION BY BQE.IDEmploye, BQE.JourSemaine ORDER BY BQE.TypeQuart ASC) AS MaxJOur
 			FROM basequartsemploye BQE
 			INNER JOIN baseemployes BE ON BE.IDEmploye=BQE.IDEmploye 
-				AND BE.IDEmployeur='Gestion8768'
+				AND BE.IDEmployeur='${employeur}'
   				AND Disponibilite='1'
   				AND ParamType='1'
 			LEFT JOIN Tableconges TC ON TC.IDEmploye=BQE.IDEmploye 
 				AND TC.JourSemaine=BQE.JourSemaine 
 				AND TC.TypeQuart=BQE.TypeQuart
-				AND TC.DateConges='2019-10-7'
+				AND TC.DateConges='${choixdate}'
 			WHERE TC.IDEmploye IS NULL 
 	)A	
 	WHERE MaxJour=1
@@ -41,7 +40,7 @@ try {
 	ORDER BY B.DateEmbauche ASC, B.JourSemaine ASC, B.TypeQuart ASC
 )C
 INNER JOIN BaseQuartsEmployeur BQER ON BQER.IDEmployeur=C.IDEmployeur
-		AND BQER.IDTableHoraire='001'
+		AND BQER.IDTableHoraire='${choixsemaine}'
 		AND BQER.TypeQuart=C.TypeQuart
 		AND BQER.JourSemaine=C.JourSemaine
 		AND C.Selection <= BQER.NBREmployes
