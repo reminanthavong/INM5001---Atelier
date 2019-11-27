fd2a1f3a73a51b014e25a64d9dbf9fcd8fb57187
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-11-27 11:56:35 -0500

    Update AffichageHoraire.vue

--- src/components/resources/AffichageHoraire.vue
+++ src/components/resources/AffichageHoraire.vue
@@ -2,8 +2,8 @@
     <div class="w3-main" style="margin-left:250px">
         <div class="w3-row w3-padding-64">
             <div class="w3-twothird w3-container">
-                <h1 class="w3-text-teal">Affichage de L'horaire</h1></br>
-                <h3>Choisissez l'horaire a afficher selon :</h3></br>
+                <h1 class="w3-text-teal">Affichage de L'horaire</h1>
+                <h3>Choisissez l'horaire a afficher selon :</h3>
 
                 <table id="choixpourafficherhoraire" style="width:100%">
                     <tr>
@@ -19,9 +19,6 @@
                         </td>
                     </tr>
                 </table>
-
-                </br>
-                </br>
 
                 <table id="tabledesHoraire"></table>
             </div>
