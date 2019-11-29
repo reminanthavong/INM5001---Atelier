b91bf1b72e3d735275286f9f84b1feae38334af9
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-28 22:09:19 -0500

    Update afficherhoraire.js

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -67,50 +67,6 @@ const fonctions4  = async (req, res) => {
 
 //---------------------------------------------------------------------------------------------------------------------------------//
 
-const GenererHoraire = async (req, res) => {
-  try {
-            const client = await pool.connect()
-            const horaire = await client.query(`SELECT DISTINCT '999' AS IDTableHoraire, '10/7/2019' AS DateParam ,C.IDEmployeur,C.IDEmploye, C.JourSemaine, C.TypeQuart,c.Selection--,NBREmployes
-FROM(
-	SELECT IDEmploye,IDEmployeur,JourSemaine, TypeQuart,Selection,DateEmbauche
-	FROM (
-		SELECT 
-		*,ROW_NUMBER()OVER(PARTITION BY A.IDEmploye ORDER BY A.JourSemaine ASC) AS MaxSem
-		,ROW_NUMBER()OVER(PARTITION BY A.JourSemaine, A.TypeQuart ORDER BY A.TypeQuart ASC, A.JourSemaine ASC) AS Selection
-		FROM(
-			SELECT BQE.*, nbrQuartsmax,DateEmbauche ,ROW_NUMBER()OVER(PARTITION BY BQE.IDEmploye, BQE.JourSemaine ORDER BY BQE.TypeQuart ASC) AS MaxJOur
-			FROM basequartsemploye BQE
-			INNER JOIN baseemployes BE ON BE.IDEmploye=BQE.IDEmploye 
-				AND BE.IDEmployeur='Gestion8768'
-  				AND Disponibilite='1'
-  				AND ParamType='1'
-			LEFT JOIN Tableconges TC ON TC.IDEmploye=BQE.IDEmploye 
-				AND TC.JourSemaine=BQE.JourSemaine 
-				AND TC.TypeQuart=BQE.TypeQuart
-				AND TC.DateConges='2019-10-7'
-			WHERE TC.IDEmploye IS NULL 
-	)A	
-	WHERE MaxJour=1
-	)B
-	WHERE MaxSem<=nbrQuartsmax
-	ORDER BY B.DateEmbauche ASC, B.JourSemaine ASC, B.TypeQuart ASC
-)C
-INNER JOIN BaseQuartsEmployeur BQER ON BQER.IDEmployeur=C.IDEmployeur
-		AND BQER.IDTableHoraire='001'
-		AND BQER.TypeQuart=C.TypeQuart
-		AND BQER.JourSemaine=C.JourSemaine
-		AND C.Selection <= BQER.NBREmployes
-
-
-;`);
-            res.json(horaire);
-            client.release();
-          } catch (err) {
-            console.error(err);
-            res.send("Erreur appel client " + err);
-          }
-}
-
 //---------------------------------------------------------------------------------------------------------------------------------//
 
 
@@ -122,5 +78,4 @@ recupererHoraire,
   fonctions2,
   fonctions3,
   fonctions4,
-  GenererHoraire
 }
