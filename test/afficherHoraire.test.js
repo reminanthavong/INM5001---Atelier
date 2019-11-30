038191e7a2b617abc76c5445c98c6497a56f3fd7
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-30 15:26:11 -0500

    Update afficherHoraire.test.js

--- test/afficherHoraire.test.js
+++ test/afficherHoraire.test.js
@@ -43,7 +43,7 @@ describe('test de AfficherHoraire', function (done) {
     //const recupererListeSemaineSTUB = sinon.stub(AfficherHoraire,'recupererListeSemaine');
     await AfficherHoraire.fonctions1(req, res); 
     sinon.assert.calledOnce(res.json); //res.json a ete appellee une fois
-    sinon.assert.notCalled(recupererListeSemaineSTUB);
+    sinon.assert.calledOnce(recupererListeSemaineSTUB);
     //sinon.assert.calledWithExactly(recupererListeSemaineSTUB, null); //recupererListeSemaine a bien ete appelle dans fonctions1
     });
     });   
