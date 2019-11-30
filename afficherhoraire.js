702a1463ebd0a170bf3a877d41047722658304af
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-30 15:39:45 -0500

    Update afficherhoraire.js

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -1,24 +1,9 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-
-async function recupererListeSemaine() {
-	    console.log('inside')
-            const client = await pool.connect()
-            const choixSemaine = await client.query(`SELECT DISTINCT IDTableHoraire FROM TableHoraire;`);
-            const choixSemaines = { 'choixSemaines': (choixSemaine) ? choixSemaine.rows : null};
-	    client.release();
-            //console.log(choixSemaines);
-            return choixSemaines;	
-}
+const Ressources = require('./ressources')
 
 async function fonctions1(req, res)  {
   try {
 	  console.log('before')
-           const choixSemaines = recupererListeSemaine();
+           const choixSemaines = await Ressources.recupererListeSemaine();
 	   console.log('after')
 
             res.json( choixSemaines );
@@ -28,31 +13,13 @@ async function fonctions1(req, res)  {
           }
 }
 
-async function recupererHoraire(choixsemaine,choixdate,employeur) {
-            const client = await pool.connect()
-            const horaires = await client.query(`SELECT *
-                                                         FROM
-                                                         (SELECT TC2.Valeur AS TypeQuart, TC3.Valeur AS JourSemaine, CONCAT(BE.NomEmploye,' ',BE.PrenomEmploye) AS NomEmploye
-                                                                                                    	FROM TableHoraire TH
-                                                                                                    	LEFT JOIN BaseEmployes BE ON BE.IDEmploye=TH.IDEmploye
-                                                                                                    	LEFT JOIN TableCodes TC2 ON (TC2.Label='TypeQuart' AND TH.TypeQuart=TC2.Code)
-                                                                                                    	LEFT JOIN TableCodes TC3 ON (TC3.Label='JourSemaine' AND TH.JourSemaine=TC3.Code)
-                                                                                                    	WHERE (TH.IDTableHoraire='${choixsemaine}' OR TH.DateParam='${choixdate}')
-                                                                                                      AND (TH.IDEmployeur='${employeur}' OR TH.IDEmployeur=(SELECT DISTINCT IDEmployeur FROM TableHoraire WHERE IDEmploye = '${employeur}') )
-                                                                                                    ) AS SourceTable;
-
-                                                         `);
-             const horairesRecu = { 'horaires': (horaires) ? horaires.rows : null};
-             client.release();
-            return horairesRecu;	
-}
 const fonctions2  = async (req, res) => {
         const resp = JSON.parse(req.body); //{$choixsemaine}
         const choixsemaine = resp['choixsemaine'] || '000';
         const choixdate = resp['choixdate'] || '01-01-1899';      
         const employeur = req.session.username//'Gestion3525' //'JNASH'// 
          try {
-		const horairesRecu= await recupererHoraire(choixsemaine,choixdate,employeur)
+		const horairesRecu= await Ressources.recupererHoraire(choixsemaine,choixdate,employeur)
                 res.json( horairesRecu );
              } catch (err) {
                console.error(err);
@@ -61,24 +28,16 @@ const fonctions2  = async (req, res) => {
 }
    
 const fonctions3  = async (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/pages/AffichageHoraire.html' /*, getHoraires */));
+  res.sendFile(path.join(__dirname+'/views/pages/AffichageHoraire.html'));
 }
 
 const fonctions4  = async (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/pages/AffichageHoraire.html' /*, getHoraires */));
+  res.sendFile(path.join(__dirname+'/views/pages/AffichageHoraire.html'));
 }
 
-
 //---------------------------------------------------------------------------------------------------------------------------------//
-
-//---------------------------------------------------------------------------------------------------------------------------------//
-
-
-
 module.exports = {
-	recupererListeSemaine,
 	fonctions1,
-	recupererHoraire,
   	fonctions2,
   	fonctions3,
   	fonctions4
