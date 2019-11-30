1b20b95605e2c634daa97eb7614d6a61e2164747
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-30 15:53:57 -0500

    Update afficherHoraire.test.js

--- test/afficherHoraire.test.js
+++ test/afficherHoraire.test.js
@@ -54,7 +54,7 @@ describe('test de AfficherHoraire', function (done) {
          expect(horaire).to.deep.equal(Horaire00120191007Gestion3525);
       });
      it('devrait retourner vrai', async function() {
-    const req = mockReq({body:{}});
+    const req = mockReq();
     const res = mockRes();
     recupererHoraireSTUB = sinon.stub(Ressources,'recupererHoraire');
     await AfficherHoraire.fonctions2(req, res); 
