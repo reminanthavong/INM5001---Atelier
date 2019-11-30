7898c7df8856ed6330599e0f5aa90f48a7aa6505
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-11-30 16:18:31 -0500

    Update afficherhoraire.js

--- afficherhoraire.js
+++ afficherhoraire.js
@@ -11,6 +11,7 @@ async function fonctions1(req, res)  {
 }
 
 const fonctions2  = async (req, res) => {
+	console.log(req.body);
         const resp = req.body; //{$choixsemaine}
         const choixsemaine = resp['choixsemaine'] || '000';
         const choixdate = resp['choixdate'] || '01-01-1899';      
