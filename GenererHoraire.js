5ca6a99c63bd2fcf3906cbd97d181851b5b01101
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-01 17:54:37 -0500

    Update GenererHoraire.js

--- GenererHoraire.js
+++ GenererHoraire.js
@@ -44,24 +44,10 @@ INNER JOIN BaseQuartsEmployeur BQER ON BQER.IDEmployeur=C.IDEmployeur
 	return horairegenere
 }
 //---------------------------------------------------------------------------------------------------------------------------------//
-const GenererHoraireReponse = async (req, res) => {
-	const resp = req.body; 
-	const choixsemaine = resp['choixsemaine'] || '000';
-        const choixdate = resp['choixdate'] || '01-01-1899';     
-        const employeur = req.session.username
-try {
-	const horaire =await GenererHoraire(choixsemaine,choixdate,employeur);
-	res.json(horaire);
- } catch (err) {
-	console.error(err);
-	res.send("Erreur appel client " + err);
- }
-}
 
 //---------------------------------------------------------------------------------------------------------------------------------//
 
 module.exports = {
 GenererHoraire,
-GenererHoraireReponse
 }
 
