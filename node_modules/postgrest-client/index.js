c0fb3c0b135f300f1ee2af4dde2873d8c97584bb
reminanthavong <souk.remi@gmail.com>
2019-12-04 10:11:33 -0500

    Migration

--- node_modules/postgrest-client/index.js
+++
@@ -1,7 +0,0 @@
-var PostgREST = require('./dist/lib/Api').default
-
-PostgREST.default = PostgREST
-PostgREST.Api = require('./dist/lib/Api').default
-PostgREST.ApiRequest = require('./dist/lib/ApiRequest').default
-
-module.exports = PostgREST
