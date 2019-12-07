98d3e3c2ca9ac0838c4682d32d64008d7a186325
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-07 16:28:16 -0500

    Update gestionEmploye.test.js

--- test/gestionEmploye.test.js
+++ test/gestionEmploye.test.js
@@ -8,40 +8,8 @@ const expect = require('chai').expect;
 //***********************************************************//
 const GestionEmploye = require('../src/backend/gestionemploye'); 
 //***********************************************************//
-const EmployesDeGestion3525=       [
-         {
-           idemployeur: 'Gestion3525',
-           idemploye: 'EBECK',
-           nomemploye: 'ELAINE R',
-           prenomemploye: 'BECKER',
-           nbrquartsmax: 40,
-           dateembauche: '1970-04-07'
-         },
-         {
-           idemployeur: 'Gestion3525',
-           idemploye: 'JNASH',
-           nomemploye: 'JENISSA C',
-           prenomemploye: 'NASHEL',
-           nbrquartsmax: 40,
-           dateembauche: '1979-04-16'
-         },
-         {
-           idemployeur: 'Gestion3525',
-           idemploye: 'MSLAT',
-           nomemploye: 'MASCHEAL',
-           prenomemploye: 'SLATTON',
-           nbrquartsmax: 40,
-           dateembauche: '2000-04-17'
-         },
-         {
-           idemployeur: 'Gestion3525',
-           idemploye: 'DPURN',
-           nomemploye: 'DESRI M',
-           prenomemploye: 'PURNELL',
-           nbrquartsmax: 40,
-           dateembauche: '1984-10-16'
-         }
-       ]
+var TestScenarii = require('./TestScenarii/GestionEmployeTests.json');
+const EmployesDeGestion3525 = TestScenarii.EmployesDeGestion3525
 //***********************************************************//
 describe('test de GestionEmploye', function (done) {
  /**********/
