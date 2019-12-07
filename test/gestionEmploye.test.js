180ca7f1d978fda1436ac37ad87657ba4f92fbe6
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-07 16:20:47 -0500

    Update gestionEmploye.test.js

--- test/gestionEmploye.test.js
+++ test/gestionEmploye.test.js
@@ -8,7 +8,41 @@ const expect = require('chai').expect;
 //***********************************************************//
 const GestionEmploye = require('../src/backend/gestionemploye'); 
 //***********************************************************//
-
+const EmployesDeGestion3525=       [
+         {
+           idemployeur: 'Gestion3525',
+           idemploye: 'EBECK',
+           nomemploye: 'ELAINE R',
+           prenomemploye: 'BECKER',
+           nbrquartsmax: 40,
+           dateembauche: '1970-04-07'
+         },
+         {
+           idemployeur: 'Gestion3525',
+           idemploye: 'JNASH',
+           nomemploye: 'JENISSA C',
+           prenomemploye: 'NASHEL',
+           nbrquartsmax: 40,
+           dateembauche: '1979-04-16'
+         },
+         {
+           idemployeur: 'Gestion3525',
+           idemploye: 'MSLAT',
+           nomemploye: 'MASCHEAL',
+           prenomemploye: 'SLATTON',
+           nbrquartsmax: 40,
+           dateembauche: '2000-04-17'
+         },
+         {
+           idemployeur: 'Gestion3525',
+           idemploye: 'DPURN',
+           nomemploye: 'DESRI M',
+           prenomemploye: 'PURNELL',
+           nbrquartsmax: 40,
+           dateembauche: '1984-10-16'
+         }
+       ]
+//***********************************************************//
 describe('test de GestionEmploye', function (done) {
  /**********/
    describe('creerIdEmploye', function (done) {
@@ -33,7 +67,7 @@ describe('test de GestionEmploye', function (done) {
    it('devrait retourner ???', async () => {
          const Employes = await GestionEmploye.getEmployes('Gestion3525');
     console.log(Employes)
-         expect(Employes).to.deep.equal('');
+         expect(Employes).to.deep.equal(EmployesDeGestion3525);
       });
    });
   /**********/
