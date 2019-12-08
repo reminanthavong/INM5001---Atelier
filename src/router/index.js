26e9d6411a97181f3e69958cc3caac35e9304b7c
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-08 18:23:57 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,7 @@ const router=new VueRouter({
 
 router.beforeEach((to, from, next) => {
       if (to.matched.some(record => record.meta.requiresAuth)) {
+          alert(store.getters.userStatus)
         if (store.getters.userStatus) {
           next()
           return
