e163c665c121bc822c9413dd8127079df3071abe
kaykay89 <kaylalouis@hotmail.com>
2019-12-08 11:33:47 -0500

    Debug afficher horaire

--- src/backend/afficherhoraire.js
+++ src/backend/afficherhoraire.js
@@ -16,22 +16,25 @@ async function afficherChoixHoraire(req, res)  {
 }
 
 const afficherHoraire  = async (req, res) => {
-        const resp = req.body; //{$choixsemaine}
-	    console.log(resp)
-        const choixsemaine = resp.choixsemaine || '000';
-        const choixdate = resp.dateHoraire || '01-01-1899';
-        const employeur = req.session.idgestion //'Gestion3525' //'JNASH'// 
-         try {
-		let horairesRecu= await Ressources.recupererHoraire(choixsemaine,choixdate.slice(0, 10),employeur)
+    const resp = req.body; //{$choixsemaine}
+    console.log(resp)
+    const choixsemaine = resp.choixsemaine || '000';
+    const choixdate = resp.dateHoraire || '01-01-1899';
+    const employeur = req.session.idgestion //'Gestion3525' //'JNASH'//
+    console.log(choixsemaine)
+    console.log(choixdate)
+    console.log(employeur)
+    try {
+		let horairesRecu = await Ressources.recupererHoraire(choixsemaine,choixdate.slice(0, 10),employeur)
 		let result = {};
 		result = horairesRecu.horaires
-                console.log(JSON.stringify(result))
+        console.log(JSON.stringify(result))
 		res.setHeader("content-type", "application/json")
 		res.send(JSON.stringify(result));
-             } catch (err) {
-               console.error(err);
-               res.send("Erreur appel client " + err);
-             }
+    } catch (err) {
+        console.error(err);
+        res.send("Erreur appel client " + err);
+    }
 }
    
 const GenererHoraireReponse = async (req, res) => {
