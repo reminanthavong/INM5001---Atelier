224b6a57db41198b2ab7d6f943feeb6e2a0d0dab
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-08 00:54:20 -0500

    Update gestionemploye.js

--- src/backend/gestionemploye.js
+++ src/backend/gestionemploye.js
@@ -14,6 +14,7 @@ const afficherEmployes  = async (req, res) => {
 	  var sessEmployeur = req.session.username;
 	  const rows = await getEmployes(sessEmployeur);
 	  res.setHeader("content-type", "application/json")
+	  console.log(JSON.stringify(rows))
 	  res.send(JSON.stringify(rows))
 }
 
