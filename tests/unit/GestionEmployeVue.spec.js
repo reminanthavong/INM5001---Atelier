a10923a7418909084db51e28ad8b4dde3d54189a
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-08 21:17:32 -0500

    Update GestionEmployeVue.spec.js

--- tests/unit/GestionEmployeVue.spec.js
+++ tests/unit/GestionEmployeVue.spec.js
@@ -7,9 +7,9 @@ test('GestionEmploye', () => {
   const wrapper = shallowMount(GestionEmploye)
 
   // Le bouton genere l'affichage
-  wrapper.find('button')[0].trigger('click')
+  wrapper.find('#testbouton1').trigger('click')
   expect(wrapper.find('bouton-affichage').text()).toEqual("Veuillez cliquer sur un employé pour le modifier")
-  wrapper.find('button')[0].trigger('click')
+  wrapper.find('#testbouton1').trigger('click')
   expect(wrapper.find('bouton-affichage').exists()).toBeFalsy()
 
   // assert the error is rendered
