82e040be56ae5c6acb0423b92024c5c7ac5423a2
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-08 22:37:22 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
     <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
       <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-theme-l1" to="/">Home</router-link> </span>
       <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/affichageHoraire">Afficher Horaire</router-link> </span>
-      <span v-if=" isAdmin=="1" "><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionHoraire">Gestion Horaire</router-link> </span>
-     <span v-if=" isAdmin=="1" "> <router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionEmployes">Gestion Employes</router-link> </span>
-      <span v-if=" isAdmin=="0" "><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/zoneEmploye">Zone Employes</router-link> </span>
+      <span v-if=" isAdmin=='1' "><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionHoraire">Gestion Horaire</router-link> </span>
+     <span v-if=" isAdmin=='1' "> <router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionEmployes">Gestion Employes</router-link> </span>
+      <span v-if=" isAdmin=='0' "><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/zoneEmploye">Zone Employes</router-link> </span>
       <span v-if="isLoggedIn">  <a class="w3-bar-item w3-button w3-hide-small w3-hover-white" @click="logout">Deconnexion </a></span>
       <span v-else>  <router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/login">Connexion</router-link></span>
     </div>
