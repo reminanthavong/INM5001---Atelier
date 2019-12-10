966a1fc35321bf009ebeb8e54b0395aa1755337d
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-09 23:18:47 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,7 @@ import axios from 'axios'
                   // Add the following line:
                   axios.defaults.headers.common['Authorization'] = token
                   commit('auth_success', token, user)
-                  alert(this.state.user)
+                  alert(user)
                   resolve(resp)
                 })
                 .catch(err => {
