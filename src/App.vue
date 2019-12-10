9bb5fcfa6953e7b6384dc1f62d615c0671cc428e
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-09 22:34:20 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
       <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-theme-l1" to="/">Home</router-link></span>
       <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/affichageHoraire">Afficher Horaire</router-link></span>
-      <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionHoraire">Gestion Horaire</router-link></span>
+      <span v-if="isAdmin"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionHoraire">Gestion Horaire</router-link></span>
       <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/gestionEmployes">Gestion Employes</router-link></span>
      <span v-if="isLoggedIn"><router-link class="w3-bar-item w3-button w3-hide-small w3-hover-white" to="/zoneEmploye">Zone Employes</router-link></span>
      <span v-if="isLoggedIn"><a class="w3-bar-item w3-button w3-hide-small w3-hover-white" @click="logout">Deconnexion </a></span>
@@ -23,6 +23,9 @@
       computed: {
         isLoggedIn: function() {
           return this.$store.getters.isLoggedIn;
+        },
+        isAdmin: function() {
+          return this.$store.getters.userData;
         }
       },
       methods: {
