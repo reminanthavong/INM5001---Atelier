d34b3bc04416df740d62d4dbc0570b97238eedd1
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-09 23:14:11 -0500

    Update login.js

--- src/backend/login.js
+++ src/backend/login.js
@@ -33,8 +33,8 @@ const loginAPI = async (request, response) => {	// Nom de la fonction
 	                        request.session.prenom = 'Administrateur'; // Valeur prenom de l'employe
 	                        request.session.typeutilisateur = checkUsername[0].typeutilisateur; // Si utilisateur est admin
 				let token = jwt.sign({ id: username }, config.secret, { expiresIn: 86400 });
-				      console.log(request.session.typeutilisateur)
-                                response.status(200).send({ auth: true, token: token, user: request.session.typeutilisateur});
+				     
+                                response.status(200).send({ auth: true, token: token, user: JSON.stringify(request.session)});
 			      }  else {
 	                        const infoUser = await getIDgestion(username); // Allez chercher les informations du utilisateur   
 				// Ajout dans JSON Session
@@ -45,8 +45,8 @@ const loginAPI = async (request, response) => {	// Nom de la fonction
 	                        request.session.prenom = infoUser[0].prenomemploye; // Valeur prenom de l'employe
 	                        request.session.typeutilisateur = checkUsername[0].typeutilisateur; // Si utilisateur est admin
 				let token = jwt.sign({ id: username }, config.secret, { expiresIn: 86400});
-				      console.log(request.session.typeutilisateur)
-                                response.status(200).send({ auth: true, token: token, user: request.session.typeutilisateur});
+				     
+                                response.status(200).send({ auth: true, token: token, user: JSON.stringify(request.session)});
 			      }
 	      
 			} else {
