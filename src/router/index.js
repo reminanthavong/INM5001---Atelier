fb3b552491a48443014c29e40b0f0a4a28b55b53
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-10 21:03:26 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const routes=[
         }       
        // alert('Condition fail')  
           next('/unauthorized')   
-      } else {
+       else {
         //  alert('Condition bypasser')
         next()
       }
