c398d366e64b4ee1e6488456d2e5318befff24f2
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-10 22:43:49 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <div class="w3-main" style="margin-left:250px">
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
-      <h1 class="w3-text-teal">Bienvenue!</h1>
+      <h1 class="w3-text-teal">Bienvenue! {{ prenom }} {{ nom }}</h1>
       <p>Veuillez vous référer aux onglets ci-dessus pour naviguer dans le site.</p>
     </div>
   </div>
@@ -10,5 +10,16 @@
 </template>
 
 <script>
-
+export default {
+        data() {
+          return {
+            nom: '',
+            prenom: ''
+          };
+        },
+        mounted() {
+              this.nom = localStorage.getItem('nom')
+              this.prenom = localStorage.getItem('prenom')
+        }
+      };
 </script>
