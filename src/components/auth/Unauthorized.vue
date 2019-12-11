9017a21b60609f84123b351bceea2137330124aa
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-10 22:32:47 -0500

    Update Unauthorized.vue

--- src/components/auth/Unauthorized.vue
+++ src/components/auth/Unauthorized.vue
@@ -3,7 +3,7 @@
     <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
       <h1 class="w3-text-teal">Non autorisé!</h1>
-      <h2 class="w3-text-teal">Vous n'avez pas l'accès à cette page.</h2>
+      <h2 class="w3-text-teal">Vous n'avez pas accès à cette page.</h2>
 	</div>
 	</div>	 
 </div>
