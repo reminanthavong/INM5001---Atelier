b2c7027f085f656703c40e5a447f37c30c85ce76
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-12 17:17:56 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+
 <div class="w3-top">
     <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
@@ -14,7 +14,7 @@
     
     </div>
 
-    <div>
+  
 <router-view><router-view/>
 </template>
 
