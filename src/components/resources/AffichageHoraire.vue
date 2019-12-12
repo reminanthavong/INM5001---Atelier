5f5bf6d209aeeec7ec528afac2339fcc1797d8ec
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-12 00:36:50 -0500

    Update AffichageHoraire.vue

--- src/components/resources/AffichageHoraire.vue
+++ src/components/resources/AffichageHoraire.vue
@@ -15,7 +15,7 @@
                 <p><b>Choisir par date:</b></p>
                 <datepicker v-model="datehoraire" name="datehoraire"></datepicker>
                 
-                <button @change="afficherHoraireSelonDate()" class="btn btn-primary">Afficher</button>
+                <button @click="afficherHoraireSelonDate()" class="btn btn-primary">Afficher</button>
                 <p></p>
                 <p v-if="afficherHoraire">Voici l'horaire</p>
                 <table id="horaire" v-if="afficherHoraire">
