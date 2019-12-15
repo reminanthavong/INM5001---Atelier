02a24d5236d9c032097daebbff587b15106a9289
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-15 16:56:23 -0500

    Update afficherHoraire.test.js

--- test/afficherHoraire.test.js
+++ test/afficherHoraire.test.js
@@ -16,6 +16,7 @@ describe('test de AfficherHoraire', function (done) {
    describe('recupererListeSemaine', function (done) {
    it('devrait retourner 001', async () => {
          const listeSemaine = await Ressources.recupererListeSemaine();
+    console.log(listeSemaine);
          expect(listeSemaine).to.deep.equal({choixSemaines: [{ idtablehoraire: '001' }]});
       });
    });
