b1e1e64ecc2602913b0dc770f3d909db4abc1c70
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-16 13:40:37 -0500

    Update afficherHoraire.test.js

--- test/afficherHoraire.test.js
+++ test/afficherHoraire.test.js
@@ -44,8 +44,7 @@ describe('test de AfficherHoraire', function (done) {
     describe('recupererHoraire', function (done) {
    it('devrait retourner le bon horaire', async () => {
          const horaire = await Ressources.recupererHoraire('001','2019-10-07','Gestion3525');
-    console.log(horaire);
-         //expect(horaire).to.deep.equal(TestScenarii.Horaire00120191007Gestion3525);
+         expect(horaire).to.deep.equal(TestScenarii.Horaire00120191007Gestion3525);
       });
      it('devrait retourner vrai', async function() {
     const req = mockReq({ body: {}});
@@ -61,8 +60,7 @@ describe('test de AfficherHoraire', function (done) {
 describe('GenererHoraire', function (done) {
     it('devrait retourner le bon horaire', async () => {
         const horaire = await Ressources.genererHoraire('001','2019-10-07','Gestion3525');
-     console.log(horaire);
-        //expect(horaire).to.deep.equal(TestScenarii.HoraireGenere00120191007Gestion3525);
+        expect(horaire).to.deep.equal(TestScenarii.HoraireGenere00120191007Gestion3525);
     });
       afterEach(() => {
         sinon.restore();
