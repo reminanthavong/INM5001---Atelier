588c422db24576fbfa201ea5aedb92771832d49f
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-15 21:47:06 -0500

    Update AffichageHoraireVue.spec.js

--- tests/unit/AffichageHoraireVue.spec.js
+++ tests/unit/AffichageHoraireVue.spec.js
@@ -3,7 +3,7 @@ import AffichageHoraire from "../../src/components/resources/AffichageHoraire.vu
 import sinon from "sinon";
 
 describe("AffichageHoraire", () => {
-  xit("Appelle afficherHoraireSelonDate quand le bouton est clique", () => {
+  it("Appelle afficherHoraireSelonDate quand le bouton est clique", () => {
     sinon.stub(AffichageHoraire.mounted);
     const spyAffichageEmployes = sinon.spy(
       AffichageHoraire.methods,
