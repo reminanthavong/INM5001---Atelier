a0eacea00ef73f5715f86bb062ad716dc1dc9447
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-16 11:07:11 -0500

    Update gestionhoraire.js

--- src/backend/gestionhoraire.js
+++ src/backend/gestionhoraire.js
@@ -1,4 +1,4 @@
-const creationHoraire = require('./ressource');
+const creationHoraire = require('./ressources');
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
