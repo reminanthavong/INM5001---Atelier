49779a51cdd086d6ca73da7f3b6a9003cd32df4a
MouradLatoui <46796941+MouradLatoui@users.noreply.github.com>
2019-12-16 13:39:38 -0500

    Update AfficherHoraireTests.js

--- test/TestScenarii/AfficherHoraireTests.js
+++ test/TestScenarii/AfficherHoraireTests.js
@@ -1,67 +1,94 @@
-var Horaire00120191007Gestion3525 = { horaires:
-          [ { typequart: 'Nuit',
-              joursemaine: 'Mercredi',
-              nomemploye: 'ELAINE R BECKER' },
-            { typequart: 'Jour',
-              joursemaine: 'Jeudi',
-              nomemploye: 'JENISSA C NASHEL' },
-            { typequart: 'Soir',
-              joursemaine: 'Jeudi',
-              nomemploye: 'MASCHEAL SLATTON' },
-            { typequart: 'Nuit',
-              joursemaine: 'Jeudi',
-              nomemploye: 'DESRI M PURNELL' } ] }
-var HoraireGenere00120191007Gestion3525 =  {horaires:
-          [ { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'DPURN',
-              joursemaine: '4',
-              typequart: 'N',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'EBECK',
-              joursemaine: '3',
-              typequart: 'S',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'EBECK',
-              joursemaine: '6',
-              typequart: 'N',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'EBECK',
-              joursemaine: '7',
-              typequart: 'J',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'JNASH',
-              joursemaine: '4',
-              typequart: 'S',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'JNASH',
-              joursemaine: '6',
-              typequart: 'S',
-              selection: '1' },
-            { idtablehoraire: '001',
-              dateparam: '2019-10-07',
-              idemployeur: 'Gestion3525',
-              idemploye: 'JNASH',
-              joursemaine: '7',
-              typequart: 'J',
-              selection: '1' } ]
-}
+var Horaire00120191007Gestion3525 = {
+         horaires: [
+           {
+             typequart: 'Jour',
+             joursemaine: 'Jeudi',
+             nomemploye: 'JENISSA C NASHEL'
+           },
+           {
+             typequart: 'Nuit',
+             joursemaine: 'Jeudi',
+             nomemploye: 'DESRI M PURNELL'
+           },
+           {
+             typequart: 'Nuit',
+             joursemaine: 'Mercredi',
+             nomemploye: 'ELAINE R BECKER'
+           },
+           {
+             typequart: 'Soir',
+             joursemaine: 'Jeudi',
+             nomemploye: 'MASCHEAL SLATTON'
+           }
+         ]
+       }
+var HoraireGenere00120191007Gestion3525 =  {
+         horaires: [
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'DPURN',
+             joursemaine: '4',
+             typequart: 'N',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'EBECK',
+             joursemaine: '3',
+             typequart: 'S',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'EBECK',
+             joursemaine: '6',
+             typequart: 'N',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'EBECK',
+             joursemaine: '7',
+             typequart: 'J',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'JNASH',
+             joursemaine: '4',
+             typequart: 'S',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'JNASH',
+             joursemaine: '6',
+             typequart: 'S',
+             selection: '1'
+           },
+           {
+             idtablehoraire: '001',
+             dateparam: '2019-10-07',
+             idemployeur: 'Gestion3525',
+             idemploye: 'JNASH',
+             joursemaine: '7',
+             typequart: 'J',
+             selection: '1'
+           }
+         ]
+       }
 
 module.exports = {
     Horaire00120191007Gestion3525,
